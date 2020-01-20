21865a8226bc535f5194261929af1400cff49509
OmarTarekAbbass <omer.tarek@ivas.com.eg>
2020-01-20 14:35:25 +0200

    occasions

--- resources/views/front/newdesignv4/home.blade.php
+++ resources/views/front/newdesignv4/home.blade.php
@@ -3,62 +3,72 @@
 @section('content')
 <!-- end header-->
 <style>
-    .row {
-        margin-right: 0;
-    }
+.row {
+    margin-right: 0;
+}
 
-    .col-12 {
-        padding-right: 0;
-        /* padding-left: 0; */
-    }
+.col-12 {
+    padding-right: 0;
+    /* padding-left: 0; */
+}
 
-    .main .status .contant .filter-img i {
-        color: #7b407a;
-        position: absolute;
-        left: 11%;
-        top: 81%;
-    }
+.main .status .contant .filter-img i {
+    color: #7b407a;
+    position: absolute;
+    left: 11%;
+    top: 81%;
+}
 
-    .main .status .contant .filter-title {
-        padding-top: 20px;
-    }
+.main .status .contant .filter-title {
+    padding-top: 21px;
+}
 
-    .main .status .contant .owl-carousel.owl-three .item .heart_two {
-        left: 28%;
-        top: 74px;
-        font-size: 15px;
-    }
-    .main .status .contant{
-        background-color: #ffffff;
-    }
-    .rounded {
-    border-radius: .35rem!important;
-    }
-    .main .user_filter .use_filter_link{
-        transform: scale(0.8);
-        bottom: 32%;
+.main .status .contant .owl-carousel.owl-three .item .heart_two {
+    left: 28%;
+    top: 74px;
+    font-size: 15px;
+}
+
+.main .status .contant {
+    background-color: #ffffff;
+}
+
+.rounded {
+    border-radius: .35rem !important;
+}
+
+.main .user_filter .use_filter_link {
+    transform: scale(0.8);
+    bottom: 32%;
     left: -3%;
     width: 38%;
-    }
-    .main .status .s_title{
-        color: #454445d9;
-    }
-    .main .status .contant .filter-img i {
-        color: #fff;
-        font-size: 10px;
-    }
-    .main .owl-video .item .video-fluid{
-        max-height: 170px;
-    }
-    .filter_today1{
-        padding-left: 0px;
-    }
-    .owl-theme .owl-dots .owl-dot span{
-        background: #7d537c;
-    }
-    .main .owl-theme .owl-dots .owl-dot.active span, .main .owl-theme .owl-dots .owl-dot:hover span{
-        background-color: #7d228a;
-    }
+}
+
+.main .status .s_title {
+    color: #454445d9;
+}
+
+.main .status .contant .filter-img i {
+    color: #fff;
+    font-size: 10px;
+}
+
+.main .owl-video .item .video-fluid {
+    max-height: 170px;
+}
+
+.filter_today1 {
+    padding-left: 0px;
+}
+
+.owl-theme .owl-dots .owl-dot span {
+    background: #7d537c;
+}
+
+.main .owl-theme .owl-dots .owl-dot.active span,
+.main .owl-theme .owl-dots .owl-dot:hover span {
+    background-color: #7d228a;
+}
 </style>
 <!-- main content -->
 <div class="main">
@@ -95,14 +105,16 @@
             <div class="row">
                 <div class="filter_today1 col-8" style="margin-top: -9%;">
                     @if(\Session::has('MSISDN') && \Session::get('Status') == 'active' )
-                    <i id="{{$Rdata_today[0]->id}}" @if(check_favourtite(\Session::get('MSISDN') , $Rdata_today[0]->id)) class="far fa-heart heart active"
+                    <i id="{{$Rdata_today[0]->id}}" @if(check_favourtite(\Session::get('MSISDN') , $Rdata_today[0]->id))
+                        class="far fa-heart heart active"
                         @else class="far fa-heart heart" @endif></i>
                     @endif
 
                     <a href="{{url('newdesignv4/filter/'.$Rdata_today[0]->id.'/'.UID())}}">
 
                         <div class="filter-img">
-                            <img class="img-fluid" src="{{url('/'.$Rdata_today[0]->path)}}" alt="today filter" style="margin-top: 13%">
+                            <img class="img-fluid" src="{{url('/'.$Rdata_today[0]->path)}}" alt="today filter"
+                                style="margin-top: 13%">
                             <i class="fas fa-heart fa-lg ajax_call" value="{{$Rdata_today[0]->id}}"></i>
                         </div>
                     </a>
@@ -129,15 +141,14 @@
 
             <div class="col-12">
                 <div class="contant rounded">
-                    @if(count($sliders) < 2)
-                    <div class="owl-one owl-carousel owl-theme">
+                    @if(count($sliders) < 2) <div class="owl-one owl-carousel owl-theme">
                         @elseif(count($sliders) == 2)
                         <div class="owl-two owl-carousel owl-theme">
                             @else
                             <div class="owl-three owl-carousel owl-theme">
                                 @endif
 
-                                @foreach ($sliders as $suggest)
+                                <!-- @foreach ($sliders as $suggest)
                                 <div class="item m-1">
                                     @if(\Session::has('MSISDN') && \Session::get('Status') == 'active' )
                                     <i id="{{$suggest->id}}" @if(check_favourtite(\Session::get('MSISDN') , $suggest->id)) class="far fa-heart heart active"
@@ -145,8 +156,19 @@
                                     @endif
                                     <a href="{{url('newdesignv4/suboccasion/'.$suggest->id.'/'.UID())}}">
                                         <img class="rounded d-block m-auto" height="83" src="{{url('/'.$suggest->image)}}" alt="{{$suggest->getTranslation('title',getCode())}}">
-                                        {{-- <i class="heart_one fas fa-heart"></i> --}}
-                                        <!-- <h4 class="h6 text-center" style="color:#495057">{{$suggest->getTranslation('title',getCode())}}</h4> -->
+                                    </a>
+                                </div>
+                                @endforeach -->
+
+                                <?php $snap_Occasions = snap_Occasions() ?>
+                                @foreach($snap_Occasions as $k=> $occasion)
+                                <div class="item m-1">
+                                    @if(\Session::has('MSISDN') && \Session::get('Status') == 'active' )
+                                    <i id="{{$occasion->id}}" @if(check_favourtite(\Session::get('MSISDN') , $occasion->id)) class="far fa-heart heart active"
+                                        @else class="far fa-heart heart" @endif></i>
+                                    @endif
+                                    <a href="{{url('newdesignv4/suboccasion/'.$occasion->id.'/'.UID())}}">
+                                        <img class="rounded d-block m-auto" height="83" src="{{url('/'.$occasion->image)}}" alt="{{$occasion->getTranslation('title',getCode())}}">
                                     </a>
                                 </div>
                                 @endforeach
@@ -177,12 +199,14 @@
                                         @foreach ($Rdata_today2 as $fav)
                                         <div class="item m-1">
                                             @if(\Session::has('MSISDN') && \Session::get('Status') == 'active' )
-                                            <i id="{{$fav->id}}" @if(check_favourtite(\Session::get('MSISDN') , $fav->id)) class="far fa-heart heart active"
+                                            <i id="{{$fav->id}}" @if(check_favourtite(\Session::get('MSISDN') ,
+                                                $fav->id)) class="far fa-heart heart active"
                                                 @else class="far fa-heart heart" @endif></i>
                                             @endif
 
                                             <a href="{{url('newdesignv4/filter/'.$fav->id.'/'.UID())}}">
-                                                <img class="rounded d-block m-auto" src="{{url('/'.$fav->path)}}" alt="" height="83">
+                                                <img class="rounded d-block m-auto" src="{{url('/'.$fav->path)}}" alt=""
+                                                    height="83">
                                                 <!-- <h4 class="h6 text-center d-block m-auto" style="color:#495057;">{{$fav->getTranslation('title',getCode())}}</h4> -->
                                             </a>
                                             <!-- <i class="heart_two far fa-heart ajax_call" id="{{$fav->id}}"></i> -->
@@ -200,29 +224,29 @@
 
             @section('script')
             <script>
-                $(document).on('click', '.ajax_call', function() {
-                    console.log($(this).attr('id'))
-                    if ($(this).hasClass('fa-heart')) {
-                        $(this).removeClass('far').addClass('fas')
-                    } else {
-                        $(this).removeClass('fas').addClass('far')
-                    }
-                })
+            $(document).on('click', '.ajax_call', function() {
+                console.log($(this).attr('id'))
+                if ($(this).hasClass('fa-heart')) {
+                    $(this).removeClass('far').addClass('fas')
+                } else {
+                    $(this).removeClass('fas').addClass('far')
+                }
+            })
 
 
-                $(document).on('click', '.ajax_call', function() {
-                    if ($(this).hasClass('fa-heart')) {
-                        var str = '?id=' + $(this).attr('id') + '&type=1';
+            $(document).on('click', '.ajax_call', function() {
+                if ($(this).hasClass('fa-heart')) {
+                    var str = '?id=' + $(this).attr('id') + '&type=1';
 
-                        $.get('{{url("like_dislike/".UID())}}' + str, function(response) {
-                            $(this).removeClass('fa-heart').addClass('fa-thumbs-up')
-                        })
-                    } else {
-                        var str = '?id=' + $(this).attr('id') + '&type=2'
-                        $.get('{{url("like_dislike/".UID())}}' + str, function(response) {
-                            $(this).removeClass('fa-thumbs-up').addClass('fa-heart')
-                        })
-                    }
-                })
+                    $.get('{{url("like_dislike/".UID())}}' + str, function(response) {
+                        $(this).removeClass('fa-heart').addClass('fa-thumbs-up')
+                    })
+                } else {
+                    var str = '?id=' + $(this).attr('id') + '&type=2'
+                    $.get('{{url("like_dislike/".UID())}}' + str, function(response) {
+                        $(this).removeClass('fa-thumbs-up').addClass('fa-heart')
+                    })
+                }
+            })
             </script>
             @endsection
