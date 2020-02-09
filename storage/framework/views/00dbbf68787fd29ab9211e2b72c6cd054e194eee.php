8a58c5c17f040f57da1363d0b41a870678bed79a
omar tarek <omar.tarek@iVAS.local>
2020-02-09 14:45:29 +0200

    edit logo rotana 9/2 omar

---
+++ storage/framework/views/00dbbf68787fd29ab9211e2b72c6cd054e194eee.php
@@ -0,0 +1,67 @@
+<?php $__env->startSection('title'); ?>
+Edit Snap Image
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('PageTitle'); ?>
+Edit Snap Image
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('PageDesc'); ?>
+Edit Snap Image
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('breadcrumb'); ?>
+<li><a href="<?php echo e(url('admin/gsnap')); ?>"> Snap Images</a></li>
+<li class="active">Edit</li>
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('PageContent'); ?>
+
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <?php echo Form::model($GreetingImg,['method'=>'PATCH','files'=>true,'action'=>['GreetingSnapController@update',$GreetingImg->id]]); ?>
+
+            <?php echo Form::hidden('redirects_to', URL::previous()); ?>
+
+            <?php echo $__env->make('admin.gsnap.form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            
+            <br>
+            <br>
+            <br>            
+            <div class="form-group">
+                <button class="btn btn-labeled btn-info" type="submit"><span class="btn-label"><i class="glyphicon glyphicon-pencil"></i></span>Edit</button>
+            </div>
+
+
+            <?php echo Form::close(); ?>
+
+        </div>
+    </div>
+</div>
+<div class="row">
+    <span class="divider"></span>
+</div>
+
+
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+<script>
+
+    var token = '<?php echo e(Session::token()); ?>';
+
+    $('#occasion_list').on('change', function () {
+        $.ajax({
+            method: 'POST',
+            url: '<?php echo e(url("admin/date")); ?>',
+            data: {
+                 id: $(this).val(),
+                 _token: token
+                }
+        })
+        .done(function (data) {
+            $('#RDate').val(data.RDate);
+            $('#EXDate').val(data.EXDate);
+        });
+    });
+
+</script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('admin.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\greeting_backend_v2_php7\resources\views/admin/gsnap/edit.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d9704dde46bc7c930dd1ce70175243e127197330.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Zain Iraq</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo e(url('assets/front/zain_iraq_landing/bootstrap.min.css')); ?>">
+
+    <style>
+        body {
+            padding: 0;
+            margin: 0;
+            width: 100%;
+            height: 100%;
+            /* overflow-y: scroll; */
+        }
+
+        section.container {
+            cursor: pointer;
+            padding: 0;
+            margin: 0;
+        }
+
+        section .landing_img {
+            text-decoration: none;
+            width: 100%;
+            height: 100%;
+            background-image: url('assets/front/zain_iraq_landing/landing_page.jpg');
+            background-size: 100% 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            position: fixed;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="landing_img"></div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- ========================= Scripts Files ========================= -->
+    <!-- jQuery -->
+    <script src="<?php echo e(url('assets/front/js/jquery-3.2.1.min.js')); ?>"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo e(url('assets/front/zain_iraq_landing/bootstrap.min.js')); ?>"></script>
+    <!-- ========================= Scripts Files ========================= -->
+</body>
+
+</html>
+<?php /**PATH C:\xampp\htdocs\greeting_backend_v2_php7\resources\views/front/zain_iraq/zain_iraq_landing.blade.php ENDPATH**/ ?>
