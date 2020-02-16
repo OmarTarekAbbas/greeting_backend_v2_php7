c12595e8ed9112c8df4b4ab31edf902fb52e01a0
emad.mohamed.ivas <emad@ivas.com.eg>
2020-02-16 14:57:51 +0200

    emad, merge omar3

--- routes/web.php
+++ routes/web.php
@@ -236,12 +236,26 @@ Route::get('zain_iraq_faild', 'FrontEndController@zain_iraq_faild');
 
 /* Du Landing = SecureD */
 Route::get('du_landing/{peroid?}/{lang?}', 'HomeController@du_landing');
+Route::get('du_landing_success', 'HomeController@du_landing_success');
 Route::get('DuSecureRedirect', 'HomeController@DuSecureRedirect');
-Route::get('du_landing_rotana/{peroid?}/{lang?}', 'HomeController@du_landingrotana');
+Route::get('du_unsubc/{peroid?}/{lang?}', 'HomeController@du_unsubc');
+Route::post('du_unsubcr', 'HomeController@du_unsubcr');
+/* Du LandingRotana */
+Route::get('du_landingrotana/{peroid?}/{lang?}', 'HomeController@du_landingrotana');
+Route::get('du_landingrotana', 'HomeController@du_landing_successrotana');
+Route::get('du_landingrotana', 'HomeController@DuSecureRedirectrotana');
+Route::get('du_unsubcrotana/{peroid?}/{lang?}', 'HomeController@du_unsubcrotana');
+Route::post('du_unsubcrrotana', 'HomeController@du_unsubcrrotana');
+/* Design4 purple  */
+Route::get('du_landing_v2/{peroid?}/{lang?}', 'HomeController@du_landing_v2');
+Route::get('du_landing_v2', 'HomeController@du_landing_success_v2');
+Route::get('du_landing_v2', 'HomeController@DuSecureRedirect_v2');
 
 
-Route::get('du_pinCode', 'FrontEndController@du_pinCode');
-Route::get('du_unsub', 'FrontEndController@du_unsub');
+
+
+
+
 
 
 //Mobily Notification
