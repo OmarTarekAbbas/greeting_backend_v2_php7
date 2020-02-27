b8619fd465bf8729f7e840c1424d20ea5dcad581
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-02-27 15:03:17 +0200

    final

--- resources/views/front/rotanav2/ajaxfav.blade.php
+++ resources/views/front/rotanav2/ajaxfav.blade.php
@@ -2,7 +2,7 @@
 <div class="col-4 p-0">
   <div class="fav_list_img mb-2">
     <a href="{{url($item->snap_link)}}" target="_blank">
-      <img class="w-100 m-auto d-block rotate-scale-down wow" data-wow-delay="1.6s" data-wow-duration="0.9s" data-wow-offset="100" src="{{url($item->path)}}" alt="Filter">
+      <img class="w-100 m-auto d-block rotate-scale-down wow" data-wow-delay="1.6s" data-wow-duration="0.9s" data-wow-offset="100" src="{{url($item->path)}}" alt="{{$item->title}}">
 
       <a id="{{$item->id}}" class="first_list_img_heart heart_heart active_heart" onclick="fav('{{$item->id}}');$(this).toggleClass('active_heart');" href="javascript:void(0)">
         <i class="fas fa-heart heart_heart rotate-scale-down wow" data-wow-delay="1.6s" data-wow-duration="0.9s" data-wow-offset="100"></i>
