2564255058a7cd16c43d06c1525b66b6aab51a16
Baher Talal <baher@ivas.com.eg>
2020-04-01 13:57:37 +0200

    fix responsive In ipad and pc landing_zain_ksa

--- resources/views/landing_v2/zain_ksa.blade.php
+++ resources/views/landing_v2/zain_ksa.blade.php
@@ -2,122 +2,153 @@
 <html lang="en" style="height:100%;">
 
 <head>
-    <meta charset="utf-8">
-    <!--IE Compatibility Meta-->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--Mobile Meta-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Zain KSA Snap Landing Page</title>
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/front/landing_v2')}}/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/front/landing_v2')}}/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/front/landing_v2')}}/css/main-style_zain.css">
-    <!--[if lt IE 9]>
+  <meta charset="utf-8">
+  <!--IE Compatibility Meta-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--Mobile Meta-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Zain KSA Snap Landing Page</title>
+  <link rel="stylesheet" type="text/css" href="{{ url('assets/front/landing_v2')}}/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="{{ url('assets/front/landing_v2')}}/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="{{ url('assets/front/landing_v2')}}/css/main-style_zain.css">
+  <!--[if lt IE 9]>
       <script src="js/html5shiv.min.js"></script>
       <script src="js/respond.min.js"></script>
       <![endif]-->
 </head>
 
 <style type="text/css">
-    .landing_page .strip {
-        margin-top: 0;
+  .landing_page .strip {
+    margin-top: 0;
+  }
+
+  .main_container {
+    background-image: url('{{url("assets/front/landing_v2/img/BG_Pattern_v2.png")}}');
+  }
+
+  .landing_page .strip {
+    background-image: url('{{url("assets/front/landing_v2/img/strip_v2.png")}}');
+  }
+
+  @media only screen and (max-width: 1025px) {
+    .shbka .zain_viva {
+      margin-top: unset;
+      margin-bottom: unset;
     }
-    .main_container{
-        background-image: url('{{url("assets/front/landing_v2/img/BG_Pattern_v2.png")}}');
+
+    .shbka .zain_viva #zain {
+      width: 30% !important;
     }
-    .landing_page .strip {
-        background-image: url('{{url("assets/front/landing_v2/img/strip_v2.png")}}');
+  }
+
+  @media (min-width: 768px) and (max-width: 1025px) {
+    .shbka .zain_viva {
+      margin-top: unset !important;
+      margin-bottom: unset !important;
     }
+  }
 </style>
 
 <body>
-    <div class="main_container">
-        <div class="landing_page">
+  <div class="main_container">
+    <div class="landing_page">
 
-            <div class="start_video" id="video">
-                <!-- <video width="100%" autoplay muted loop="true">
+      <div class="start_video" id="video">
+        <!-- <video width="100%" autoplay muted loop="true">
                     <source src="{{ url('assets/front/landing_v2')}}/video/1.mp4" type="video/mp4">
                 </video> -->
-                <img width="100%" src="{{ url('assets/front/landing_v2')}}/img/045.jpg" alt="فلاتر">
-            </div>
+        <img width="100%" src="{{ url('assets/front/landing_v2')}}/img/045.jpg" alt="فلاتر">
+      </div>
 
-            <div class="strip">
-                <h2>استمتع بوقتك مع فلاتر</h2>
-            </div>
+      <div class="strip">
+        <h2>استمتع بوقتك مع فلاتر</h2>
+      </div>
 
-            <div class="shbka">
-                <div class="container">
-                    <h3>اشترك الان</h3>
-                    <div class="zain_viva">
-                      @if(Session::has('success'))
-                      <div class="alert alert-success alert-dismissible">
-                          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                          {{ Session::get('success')}}
-                      </div>
-                      @elseif(Session::has('failed'))
-                      <div class="alert alert-danger alert-dismissible">
-                          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                          {{ Session::get('failed')}}
-                      </div>
-                      @endif
-                        <div class="row justify-content-center">
-                            <!--<div class="col-12">
+      <div class="shbka">
+        <div class="container">
+          <h3>اشترك الان</h3>
+          <div class="zain_viva">
+            @if(Session::has('success'))
+            <div class="alert alert-success alert-dismissible">
+              <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+              {{ Session::get('success')}}
+            </div>
+            @elseif(Session::has('failed'))
+            <div class="alert alert-danger alert-dismissible">
+              <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+              {{ Session::get('failed')}}
+            </div>
+            @endif
+            <div class="row justify-content-center">
+              <!--<div class="col-12">
                                 <img src="img/viva.png" id="viva">
                             </div>-->
 
-                            <div class="col-12">
-                                {{--  <img src="{{ url('assets/front/landing_v2')}}/img/mobily.png" id="zain">  --}}
-                                <img src="{{ url('assets/front/landing_v2')}}/img/zain_ksa.png" id="zain">
-                            </div>
+              <div class="col-12">
+                {{-- <img src="{{ url('assets/front/landing_v2')}}/img/mobily.png" id="zain"> --}}
+                <img src="{{ url('assets/front/landing_v2')}}/img/zain_ksa.png" id="zain">
+              </div>
 
-                            <!--<div class="col-12">
+              <!--<div class="col-12">
                                 <img src="img/oredoo.png" id="ooredoo">
                             </div>-->
-                        </div>
-                    </div>
-                </div>
             </div>
+          </div>
+        </div>
+      </div>
 
-            <div class="container">
-                <div class="form_content">
-                    <!--<h5>ادخل رقم الهاتف</h5>-->
-                    <form method="post" action="ZainKsaPinCodeSend" id="form_zain">
-                      {{ csrf_field() }}
-                        <div class="form-group form-inline">
-                            <label for="phone"><span>{{zain_ksa_prefix}}</span></label>
-                            <input type="hidden" name="prev_url"  value="{{(isset($_REQUEST['prev_url'])?$_REQUEST['prev_url']:'')}}"  >
-                            <input type="tel" class="form-control" id="phone" value="{{$MSISDN}}" placeholder="ادخل رقم تليفونك" name="number" required pattern="[0-9]{9}">
-                            <span class="validity"></span>
-                        </div>
-                        <!--<button class="btn back">رجوع</button>-->
-                        <button id="zain_submit"  class="btn" type="submit">اشترك</button>
-                    </form>
-                    <!--<h5>للاشتراك يرجى الارسال الى <span>965</span></h5>
+      <div class="container">
+        <div class="form_content">
+          <!--<h5>ادخل رقم الهاتف</h5>-->
+          <form method="post" action="ZainKsaPinCodeSend" id="form_zain">
+            {{ csrf_field() }}
+            <div class="form-group form-inline">
+              <label for="phone"><span>{{zain_ksa_prefix}}</span></label>
+              <input type="hidden" name="prev_url" value="{{(isset($_REQUEST['prev_url'])?$_REQUEST['prev_url']:'')}}">
+              <input type="tel" class="form-control" id="phone" value="{{$MSISDN}}" placeholder="ادخل رقم تليفونك" name="number" required pattern="[0-9]{9}">
+              <span class="validity"></span>
+            </div>
+            <!--<button class="btn back">رجوع</button>-->
+            <button id="zain_submit" class="btn" type="submit">اشترك</button>
+          </form>
+          <!--<h5>للاشتراك يرجى الارسال الى <span>965</span></h5>
                 <h5>الى <span>965</span><span> STOP1 </span>لالغاء الاشتراك ارسل</h5>-->
-                </div>
-            </div>
+        </div>
+      </div>
 
-            <div class="cancel" style="color:#000;">
-                <p>اشترك في الخدمة مقابل 5 ريال اسبوعيا </p>
-            </div>
+      <div class="cancel" style="color:#000;">
+        <p>اشترك في الخدمة مقابل 5 ريال اسبوعيا </p>
+      </div>
 
-            {{--  <div class="cancel">
+      {{-- <div class="cancel">
                 <p>لالغاء الاشتراك يرجي الضغط علي هذا <a href="{{url('zain_ksa_unsub')}}">الرابط</a></p>
-            </div>    --}}
+    </div> --}}
 
 
-        </div>
+  </div>
 
-        <div class="copy">
-            <p>copyright @ <span><?php  echo date("Y")?></span> Digizone, all rights reserved.</p>
-        </div>
+  <div class="copy">
+    <p>copyright @ <span><?php echo date("Y") ?></span> Digizone, all rights reserved.</p>
+  </div>
 
 
-    </div>
-    <!-- script -->
-    <script src="{{ url('assets/front/landing_v2')}}/js/jquery-3.3.1.min.js"></script>
-    <script src="{{ url('assets/front/landing_v2')}}/js/popper.min.js"></script>
-    <script src="{{ url('assets/front/landing_v2')}}/js/bootstrap.min.js"></script>
-    <script src="{{ url('assets/front/landing_v2')}}/js/script_zain.js"></script>
+  </div>
+
+  <div class="the-frame">
+    <iframe class="full-screen-preview__frame" src="{{url()->full()}}" name="preview-frame" frameborder="0" noresize="noresize" data-view="fullScreenPreview" style="height: 570px; width: 370px; border-radius: 10px;">
+    </iframe>
+  </div>
+
+  <script>
+    if (screen.width <= 1025) {
+      // document.location.href = "{{url('front.home')}}";
+    }
+  </script>
+  <!-- script -->
+  <script src="{{ url('assets/front/landing_v2')}}/js/jquery-3.3.1.min.js"></script>
+  <script src="{{ url('assets/front/landing_v2')}}/js/popper.min.js"></script>
+  <script src="{{ url('assets/front/landing_v2')}}/js/bootstrap.min.js"></script>
+  <script src="{{ url('assets/front/landing_v2')}}/js/script_zain.js"></script>
 
 </body>
 
