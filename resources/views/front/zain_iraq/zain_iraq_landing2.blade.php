d1fc4edb78c4271d89683e3ebfaac23fda36c452
emad.mohamed.ivas <emad@ivas.com.eg>
2020-06-10 10:59:32 +0200

    emad,add zain iraq he

--- resources/views/front/zain_iraq/zain_iraq_landing2.blade.php
+++ resources/views/front/zain_iraq/zain_iraq_landing2.blade.php
@@ -1,3 +1,44 @@
+<?php
+define('ServiceKey', 'BPMgnXIBfLoxjEnE6WUx');
+define('uniqueUrlIdentifier', 'token');
+
+define('TransactionID', (isset($_GET['transaction_id']) ? $_GET['transaction_id'] : time()));
+define('APIURL', 'http://sg.apiserver.shield.monitoringservice.co/' . ServiceKey . '/' . TransactionID . '/JS');
+define('ApiSnippetUrl', 'https://uk.api.shield.monitoringservice.co/');
+$head = apache_request_headers();
+if (is_array($head) !== false) {
+    $h = urlencode(json_encode($head));
+} else {
+    $h = "";
+}
+$ctx = stream_context_create(array('http' => array('user_agent' => $_SERVER['HTTP_USER_AGENT'])));
+$params = http_build_query(array(
+    'lpu' => urlencode((isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http') . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']),
+    'timestamp' => str_replace('.', '', isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQUEST_TIME_FLOAT'] : microtime(true)),
+    'user_ip' => $_SERVER['REMOTE_ADDR'],
+    'head' => $h
+));
+$response = json_decode(file_get_contents(APIURL . "?" . $params, null, $ctx));
+if (!empty($response)) {
+    $source = $response->source;
+    $uniqid = $response->uniqid; // Unique Key To Use For Block API Call
+} else {
+    $uniqid = md5($params['user_ip'] . '-' . TransactionID . '-' . microtime(true)); // Unique Key To Use For Block API Call
+    $source = "(function(s, o, u, r, k){
+            b = s.URL;
+            a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.setAttribute('crossorigin', 'anonymous');
+            a.src = u+'script.js?ak='+k+'&lpi='+" . TransactionID . "+'&lpu='+encodeURIComponent(b)+'&key=$uniqid';
+            m.parentNode.insertBefore(a, m);
+    })(document, 'script', '" . ApiSnippetUrl . "', '" . TransactionID . "', '" . ServiceKey . "');";
+}
+?>
+
+<!-- $uniqid Will Be Used To Call Block API -->
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +47,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Zain Iraq</title>
+    <script type="text/javascript">
+        //<![CDATA[
+        <?=$source?>
+        //]]>
+    </script>
   <!-- Bootstrap -->
   <link rel="stylesheet" href="{{ url('assets/front/zain_iraq_landing/bootstrap.min.css')}}">
 
@@ -71,7 +117,7 @@
             <h6 class="h_6 p-2 text-center h5" dir="rtl">3000 سناب شات فلتر</h6>
 
             <p class="w-100 text-center mt-3 text-white">اهلا بك في خدمة فلاتر من زين العراق</p>
-            <a href="#0">
+            <a href="http://www.social-sms.com/iq/HE/v1.2/oneclick/sub.php?serviceId={}&spId={}&shortcode={}&uniqid=$uniqid">
               <img class="w-50 m-auto d-block" src="assets/front/zain_iraq_landing/zain.png" alt="">
             </a>
             <div class="h_6 text-center p-1 w-75">سوف تحصل على يوم واحد مجاني عند الاشتراك</div>
