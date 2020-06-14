0226b23b0c42bbf244394bbc8bdc63d1a2571053
emad.mohamed.ivas <emad@ivas.com.eg>
2020-06-14 15:59:12 +0200

    emad,make mobily Ksa landing log

--- app/Http/Controllers/MobilyksaController.php
+++ app/Http/Controllers/MobilyksaController.php
@@ -20,8 +20,28 @@ class MobilyksaController extends Controller
 {
 
 
-  public function landing_rotana_mobily_ksa()
+  public function landing_rotana_mobily_ksa(Request $request)
   {
+
+    $ip = $_SERVER["REMOTE_ADDR"];
+
+    if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+
+        $country_from_ip = $this->ip_info("Visitor", "Country");
+
+    $actionName = "Mobily KSA Binary landing Page";
+    $parameters_arr = array(
+      'date' => Carbon::now()->format('Y-m-d H:i:s'),
+      'ip'=> $ip ,
+      'country'=>$country_from_ip
+
+    );
+
+    $URL = $request->fullUrl();
+    $this->log($actionName, $URL, $parameters_arr);
 
     return view('landing_v2.mobily.rotana_mobily_landing');
   }
@@ -101,5 +121,75 @@ class MobilyksaController extends Controller
     $log->pushHandler(new StreamHandler(storage_path('logs/' . $date . '/' . $actionName . '/logFile.log', Logger::INFO)));
     $log->addInfo($URL, $parameters_arr);
   }
+
+  function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE)
+  {
+      $output = NULL;
+      if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+          $ip = $_SERVER["REMOTE_ADDR"];
+          if ($deep_detect) {
+              if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+              if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                  $ip = $_SERVER['HTTP_CLIENT_IP'];
+          }
+      }
+      $purpose = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+      $support = array("country", "countrycode", "state", "region", "city", "location", "address");
+      $continents = array(
+          "AF" => "Africa",
+          "AN" => "Antarctica",
+          "AS" => "Asia",
+          "EU" => "Europe",
+          "OC" => "Australia (Oceania)",
+          "NA" => "North America",
+          "SA" => "South America"
+      );
+      if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+        //  $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+
+          $ipdat  = @json_decode($this->GetPageData("http://www.geoplugin.net/json.gp?ip=" . $ip)) ;
+
+          if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+              switch ($purpose) {
+                  case "location":
+                      $output = array(
+                          "city" => @$ipdat->geoplugin_city,
+                          "state" => @$ipdat->geoplugin_regionName,
+                          "country" => @$ipdat->geoplugin_countryName,
+                          "country_code" => @$ipdat->geoplugin_countryCode,
+                          "continent" => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+                          "continent_code" => @$ipdat->geoplugin_continentCode
+                      );
+                      break;
+                  case "address":
+                      $address = array($ipdat->geoplugin_countryName);
+                      if (@strlen($ipdat->geoplugin_regionName) >= 1)
+                          $address[] = $ipdat->geoplugin_regionName;
+                      if (@strlen($ipdat->geoplugin_city) >= 1)
+                          $address[] = $ipdat->geoplugin_city;
+                      $output = implode(", ", array_reverse($address));
+                      break;
+                  case "city":
+                      $output = @$ipdat->geoplugin_city;
+                      break;
+                  case "state":
+                      $output = @$ipdat->geoplugin_regionName;
+                      break;
+                  case "region":
+                      $output = @$ipdat->geoplugin_regionName;
+                      break;
+                  case "country":
+                      $output = @$ipdat->geoplugin_countryName;
+                      break;
+                  case "countrycode":
+                      $output = @$ipdat->geoplugin_countryCode;
+                      break;
+              }
+          }
+      }
+      return $output;
+  }
+
 
 }
