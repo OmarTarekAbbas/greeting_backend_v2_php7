69c0f04202cf74be8b9f7729045f2040e1f9c521
Baher Talal <baher@ivas.com.eg>
2020-07-11 13:44:54 +0200

    search page done

--- resources/views/front/akhbar/nofilter.blade.php
+++ resources/views/front/akhbar/nofilter.blade.php
@@ -1,8 +1,8 @@
 <section class="no_filter w-100">
   <div class="col-12 mb-3 swirl-out-bck">
     <div class="sub_img">
-      <img class="w-75 d-block m-auto img_logo_st" src="{{url('assets/front/rotanav2/images/strip.png')}}" alt="strip">
-      <h6 class="text-black text-center font_not_filter w-100">{!! static_lang('nofilter')?static_lang('nofilter') : 'لا يوجد
+      <img class="w-75 d-block m-auto img_logo_st" src="{{url('assets/front/akhbar/images/Cutting/strip.png')}}" alt="strip">
+      <h6 class="text-black text-white text-center font_not_filter w-100">{!! static_lang('nofilter')?static_lang('nofilter') : 'لا يوجد
         فلاتر' !!}</h6>
     </div>
   </div>
