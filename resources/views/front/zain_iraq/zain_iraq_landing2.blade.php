3706aaa74838f1195abac8ba30f87893c32dbc30
Baher Talal <baher@ivas.com.eg>
2020-10-18 12:27:08 +0200

    zain iraq landing

--- resources/views/front/zain_iraq/zain_iraq_landing2.blade.php
+++ resources/views/front/zain_iraq/zain_iraq_landing2.blade.php
@@ -8,24 +8,24 @@ define('APIURL', 'http://sg.apiserver.shield.monitoringservice.co/' . ServiceKey
 define('ApiSnippetUrl', 'https://uk.api.shield.monitoringservice.co/');
 $head = apache_request_headers();
 if (is_array($head) !== false) {
-    $h = urlencode(json_encode($head));
+  $h = urlencode(json_encode($head));
 } else {
-    $h = "";
+  $h = "";
 }
 $ctx = stream_context_create(array('http' => array('user_agent' => $_SERVER['HTTP_USER_AGENT'])));
 $params = http_build_query(array(
-    'lpu' => urlencode((isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http') . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']),
-    'timestamp' => str_replace('.', '', isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQUEST_TIME_FLOAT'] : microtime(true)),
-    'user_ip' => $_SERVER['REMOTE_ADDR'],
-    'head' => $h
+  'lpu' => urlencode((isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http') . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']),
+  'timestamp' => str_replace('.', '', isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQUEST_TIME_FLOAT'] : microtime(true)),
+  'user_ip' => $_SERVER['REMOTE_ADDR'],
+  'head' => $h
 ));
 $response = json_decode(file_get_contents(APIURL . "?" . $params, null, $ctx));
 if (!empty($response)) {
-    $source = $response->source;
-    $uniqid = $response->uniqid; // Unique Key To Use For Block API Call
+  $source = $response->source;
+  $uniqid = $response->uniqid; // Unique Key To Use For Block API Call
 } else {
-    $uniqid = md5($params['user_ip'] . '-' . TransactionID . '-' . microtime(true)); // Unique Key To Use For Block API Call
-    $source = "(function(s, o, u, r, k){
+  $uniqid = md5($params['user_ip'] . '-' . TransactionID . '-' . microtime(true)); // Unique Key To Use For Block API Call
+  $source = "(function(s, o, u, r, k){
             b = s.URL;
             a = s.createElement(o),
             m = s.getElementsByTagName(o)[0];
@@ -49,9 +49,9 @@ if (!empty($response)) {
   <title>Zain Iraq</title>
   <script type="text/javascript">
     //<![CDATA[
-    <?=$source?>
+    <?= $source ?>
     //]]>
-</script>
+  </script>
   <!-- Bootstrap -->
   <link rel="stylesheet" href="{{ url('assets/front/zain_iraq_landing/bootstrap.min.css')}}">
 
@@ -94,6 +94,16 @@ if (!empty($response)) {
       margin-top: 1rem;
     }
 
+    .sub_now {
+      background-color: #ffefe1;
+      color: #e16d32;
+      border: 2px solid #e16d32;
+      border-top-right-radius: 30px;
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 30px;
+      border-bottom-right-radius: 5px;
+    }
+
     @media (min-width: 320px) and (max-width: 359.98px) {
       .copy {
         position: unset !important;
@@ -117,10 +127,9 @@ if (!empty($response)) {
             <h6 class="h_6 p-2 text-center h5" dir="rtl">3000 سناب شات فلتر</h6>
 
             <p class="w-100 text-center mt-3 text-white">اهلا بك في خدمة فلاتر من زين العراق</p>
-            <p class="w-100 text-center mt-3 text-white">اشترك الان بالضغط علي زين</p>
-            <a href="http://www.social-sms.com/iq/HE/v1.2/oneclick/sub.php?serviceId=1878&spId=157&shortcode=2680&uniqid={{$uniqid}}"   referrerpolicy="unsafe-url">
-              <img class="w-50 m-auto d-block" src="assets/front/zain_iraq_landing/zain.png" alt="">
-            </a>
+            <!-- <p class="w-100 text-center mt-3 text-white">اشترك الان بالضغط علي زين</p> -->
+            <a href="http://www.social-sms.com/iq/HE/v1.2/oneclick/sub.php?serviceId=1878&spId=157&shortcode=2680&uniqid={{$uniqid}}" class="sub_now text-center d-block m-auto w-50 p-1 font-weight-bold" referrerpolicy="unsafe-url">اشترك الان</a>
+            <!-- <img class="w-50 m-auto d-block" src="assets/front/zain_iraq_landing/zain.png" alt=""> -->
             <div class="h_6 text-center p-1 w-75">سوف تحصل على يوم واحد مجاني عند الاشتراك</div>
             <p class="w-75 m-auto mb-3 text-center text-white">سعر الخدمة 240 دينار عراقي للرسالة الواحدة المستلمة بعد انتهاء الفترة المجانية. لالغاء الاشترا ك ارسل 0 الى 2680</p>
           </div>
